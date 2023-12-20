0ebbb20a5b7fe20e629b655806aa45ac4062cd64
beedsneeds <bharadhwajkram@gmail.com>
2023-12-20 17:48:42 +0530

    Minor: secret key

--- HelloInternet/settings.py
+++ HelloInternet/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ['SECRET_KEY']
-SECRET_KEY='django-insecure-u#(7+^b4st*83+$@xjwx2o-l=v5_lgk82j-bnv@(@85@6=aj)v'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY='django-insecure-u#(7+^b4st*83+$@xjwx2o-l=v5_lgk82j-bnv@(@85@6=aj)v'
 
 
 
